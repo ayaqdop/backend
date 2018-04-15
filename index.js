911c47340f70d107343c939e68e59d737f26ccef
Olzhas Askar <olzhas.askar@gmail.com>
2018-04-15 21:47:57 +0200

    Removed commented code

--- index.js
+++ index.js
@@ -7,17 +7,6 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const uuidv4 = require('uuid/v4');
 
-//CORS middleware
-// const allowCrossDomain = (req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-
-//   next();
-// }
-
-
-//app.use(allowCrossDomain);
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 
@@ -30,7 +19,7 @@ io.on('connection', socket => {
     console.log("Serving messages: " + msg);
     io.emit("client", msg);
   });
-  
+
   socket.on("uuid", () => {
     const id = uuidv4();
     console.log("Asking for uuid. Here it is: " + id);
