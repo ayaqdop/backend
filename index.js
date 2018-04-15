26f82750e1d19a10f6c6c459556674ed2d3be74d
Olzhas Askar <olzhas.askar@gmail.com>
2018-04-15 21:47:28 +0200

    Temporary rewrite

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require("express");
-const http = require("http");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
+const uuidv4 = require('uuid/v4');
 
-const app = express();
+//CORS middleware
+// const allowCrossDomain = (req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
+//   next();
+// }
+
+
+//app.use(allowCrossDomain);
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 
@@ -12,10 +25,17 @@ app.get('/game/:name', (req, res, next) => {
   res.end('Hi ' + req.params.name +'!');
 });
 
-const hostname = "localhost";
-const port = 3000;
-const server = http.createServer(app);
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+io.on('connection', socket => {
+  socket.on("server", msg => {
+    console.log("Serving messages: " + msg);
+    io.emit("client", msg);
+  });
+  
+  socket.on("uuid", () => {
+    const id = uuidv4();
+    console.log("Asking for uuid. Here it is: " + id);
+    io.emit("generateUuid", id);
+  });
 });
+
+server.listen(4200);
