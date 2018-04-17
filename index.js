b0adff3978c91635e7e8d733a58e81d6ebd96a95
Olzhas Askar <olzhas.askar@gmail.com>
2018-04-17 22:37:09 +0200

    Trying to start with backend

--- index.js
+++ index.js
@@ -10,9 +10,71 @@ const uuidv4 = require('uuid/v4');
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/game/:name', (req, res, next) => {
   res.end('Hi ' + req.params.name +'!');
 });
+
+app.post("/uuid", (req, res) => {
+  res.status = 200;
+  const id = uuidv4();
+  res.send({ uuid: id });
+});
+
+app.post("/init", (req, res) => {
+  res.status = 200;
+  res.setHeader("Content-Type", "application/json");
+
+  const gameObjects = {
+    teams: [{
+      name: "FC Barcelona",
+      players: [
+        { number: 1,  position: [1, 8] },
+        { number: 18, position: [6, 3] },
+        { number: 23, position: [4, 6] },
+        { number: 3,  position: [4, 11] },
+        { number: 20, position: [6, 14] },
+        { number: 8,  position: [10, 5] },
+        { number: 5,  position: [8, 8] },
+        { number: 4,  position: [10, 12] },
+        { number: 14, position: [12, 4] },
+        { number: 9,  position: [12, 8] },
+        { number: 10, position: [12, 13] },
+      ]
+    },
+    {
+      name: "FC Bayern",
+      players: [
+        { number: 1,   position: [24, 9] },
+        { number: 32,  position: [19, 3] },
+        { number: 5,   position: [22, 7] },
+        { number: 17,  position: [22, 10] },
+        { number: 27,  position: [19, 14] },
+        { number: 25,  position: [16, 4] },
+        { number: 6,   position: [18, 9] },
+        { number: 11,  position: [16, 13] },
+        { number: 10,  position: [13, 14] },
+        { number: 9,   position: [15, 8] },
+        { number: 7,   position: [13, 3] },
+      ]
+    }]
+  };
+  gameObjects.ball = {
+    position: [12, 9]
+  };
+  gameObjects.teams.forEach(t => {
+    t.score = 0;
+    t.moves = 0;
+    t.players.forEach(p => p.moves = 0);
+  });
+
+  res.json(gameObjects);
+})
 
 io.on('connection', socket => {
   socket.on("server", msg => {
